4d680267a36bc9a2ac22f05b65e3e0e089fbd706
Aaron Appel <Aaronrappel@gmail.com>
2015-12-11 11:10:29 -0700

    Connected to Azure DB

--- SeedConnection.Web/Models/IdentityModels.cs
+++ SeedConnection.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace SeedConnection.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("SeedConnection", throwIfV1Schema: false)
         {
         }
 
